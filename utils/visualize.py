ec551c160f85a08a9cc1633b9c0b47865a33b69e
Jernej Fink <jefink@student.ethz.ch>
2016-09-17 21:43:55 +0200

    Added some stuff

--- utils/visualize.py
+++ utils/visualize.py
@@ -5,6 +5,17 @@ import datetime
 import numpy as np
 import glob
 from matplotlib import pyplot as plt
+import bisect
+import json
+import time
+
+def find_lt(a, x):
+    'Find rightmost value less than x'
+    i = bisect.bisect_left(a, x)
+    if i:
+        return i-1
+    raise ValueError
+
 
 def init():
 	if(len(sys.argv) < 4):
@@ -33,13 +44,7 @@ def init():
 			json_data[i]["recorded_at"] = datetime.datetime.fromtimestamp(row["recorded_at"])	
 		json_datasets[j] = json_data	
 
-	return option, json_datasets
-
-def get_battery_dataset(json_datasets):
-	datasets = [list(filter(lambda x : "MDI_EXT_BATT_VOLTAGE" in x["fields"], data)) for data in json_datasets]
-	x = [[data["received_at"] for data in dataset] for dataset in datasets]
-	y = [np.array([data["fields"]["MDI_EXT_BATT_VOLTAGE"] for data in dataset])/1000. for dataset in datasets]
-	return x, y
+	return car_ids, option, json_datasets
 
 def get_acc_datasets(json_datasets):
 	datasets = [list(
@@ -59,22 +64,10 @@ def get_location_datasets(json_datasets):
 	y = [[row["loc"][1] for row in dataset if row["loc"]] for dataset in json_datasets]
 	return x, y
 
-def get_rpm_averages(json_datasets):
-	keys = ["MDI_RPM_AVERAGE", "MDI_RPM_AVERAGE_RANGE_1", "MDI_RPM_AVERAGE_RANGE_2", "MDI_RPM_AVERAGE_RANGE_3", "MDI_RPM_AVERAGE_RANGE_4"]
-	datasets = [list(filter(lambda x : any((key in x["fields"] for key in keys)), data)) for data in json_datasets]
-
-	x = [[data["received_at"] for data in dataset] for dataset in datasets]	
-	avg = [np.array([row["fields"]["MDI_RPM_AVERAGE"] for row in dataset if "MDI_RPM_AVERAGE" in row["fields"]]) for dataset in datasets]
-	range_1 = [np.array([row["fields"]["MDI_RPM_AVERAGE_RANGE_1"] for row in dataset if "MDI_RPM_AVERAGE_RANGE_1" in row["fields"]]) for dataset in datasets]
-	range_2 = [np.array([row["fields"]["MDI_RPM_AVERAGE_RANGE_2"] for row in dataset if "MDI_RPM_AVERAGE_RANGE_2" in row["fields"]]) for dataset in datasets]
-	range_3 = [np.array([row["fields"]["MDI_RPM_AVERAGE_RANGE_3"] for row in dataset if "MDI_RPM_AVERAGE_RANGE_3" in row["fields"]]) for dataset in datasets]
-	range_4 = [np.array([row["fields"]["MDI_RPM_AVERAGE_RANGE_4"] for row in dataset if "MDI_RPM_AVERAGE_RANGE_4" in row["fields"]]) for dataset in datasets]
-	return x, avg, range_1, range_2, range_3, range_4
-	
 def get_dataset_by_key(json_datasets, key, time=False):
 	datasets = [list(filter(lambda x : key in x["fields"], data)) for data in json_datasets]
 	x = [[data["received_at"] for data in dataset] for dataset in datasets]	
-	y = [np.array([data["fields"][key] for data in dataset])/1000. for dataset in datasets]
+	y = [np.array([data["fields"][key] for data in dataset]) for dataset in datasets]
 
 	if(time):
 		return x, y
@@ -86,11 +79,10 @@ def get_dataset_by_key(json_datasets, key, time=False):
 
 if(__name__ == "__main__"):
 
-	option, json_datasets = init()
+	car_ids, option, json_datasets = init()
 
 	if(option == "BATTERY"):
-		x, y = get_battery_dataset(json_datasets)
-		print(len(x[0]), len(y[0]))
+		x, y = get_dataset_by_key(json_datasets, "MDI_EXT_BATT_VOLTAGE", time=True)
 		plot.plot(x, y, "Battery", "Time", "Voltage[V]")
 
 	if(option == "LOCATION"):
@@ -104,39 +96,76 @@ if(__name__ == "__main__"):
 		plot.plot(x, acc, "Acceleration peaks", "Time", "Acceleration[mG]")
 
 	if(option == "RPM_AVG"):
-		x, avg_rpm, range_1, range_2, range_3, range_4 = get_rpm_averages(json_datasets)
-		alpha = 0.5
-		plt.subplot(211)		
-		plt.hist(avg_rpm[0], bins=15, color=plot.tableau20[0], alpha=alpha, normed=True)
-		plt.hist(range_1[0], bins=15, color=plot.tableau20[2], alpha=alpha, normed=True)
-		plt.hist(range_2[0], bins=15, color=plot.tableau20[4], alpha=alpha, normed=True)
-		plt.hist(range_3[0], bins=15, color=plot.tableau20[6], alpha=alpha, normed=True)
-		plt.hist(range_4[0], bins=15, color=plot.tableau20[8], alpha=alpha, normed=True)
+		times = [[] for i in range(6)]
+		times[0], avg_rpm = get_dataset_by_key(json_datasets, "MDI_RPM_AVERAGE", time=True)
+		times[1], range_1 = get_dataset_by_key(json_datasets, "MDI_RPM_AVERAGE_RANGE_1", time=True)
+		times[2], range_2 = get_dataset_by_key(json_datasets, "MDI_RPM_AVERAGE_RANGE_2", time=True)
+		times[3], range_3 = get_dataset_by_key(json_datasets, "MDI_RPM_AVERAGE_RANGE_3", time=True)
+		times[4], range_4 = get_dataset_by_key(json_datasets, "MDI_RPM_AVERAGE_RANGE_4", time=True)
+		times[5], rpm_odb = get_dataset_by_key(json_datasets, "MDI_OBD_RPM", time=True)
 
-		plt.subplot(212)
-		plt.hist(avg_rpm[1], bins=15, color=plot.tableau20[0], alpha=alpha, normed=True)
-		plt.hist(range_1[1], bins=15, color=plot.tableau20[2], alpha=alpha, normed=True)
-		plt.hist(range_2[1], bins=15, color=plot.tableau20[4], alpha=alpha, normed=True)
-		plt.hist(range_3[1], bins=15, color=plot.tableau20[6], alpha=alpha, normed=True)
-		plt.hist(range_4[1], bins=15, color=plot.tableau20[8], alpha=alpha, normed=True)
+		alpha = 0.5
+		for i in range(len(times[0])):
+			plt.subplot(len(times[0]), 1, i+1)
+			data = [avg_rpm[i], range_1[i], range_2[i], range_3[i], range_4[i], rpm_odb[i] ]
+			colors = [plot.tableau20[2*i] for i in range(len(data))]
+			labels = ["avg", "range1", "range2", "range3", "range4", "odb"]
+			plt.hist(data, 20, normed=1, histtype='stepfilled', color=colors, label=labels, alpha=0.5)
+			plt.legend(prop={"size": 10})
 		plt.show()	
 
-	if(option == "CRASH"):
-		times = [[] for i in range(4)]
+	if(option == "RPM_FUEL"):
+		times = [[], [], []]
+		times[0], rpm_odb = get_dataset_by_key(json_datasets, "MDI_OBD_RPM", time=True)
+		times[1], consumed_measured = get_dataset_by_key(json_datasets, "MDI_OBD_FUEL", time=True)
+		times[2], mileage = get_dataset_by_key(json_datasets, "ODO_FULL", time=True) 
 
-		times[0], crash = get_dataset_by_key(json_datasets, "MDI_CRASH_DETECTED", time=True)
-		times[1], acc_x = get_dataset_by_key(json_datasets, "BEHAVE_ACC_X_PEAK", time=True)
-		times[2], acc_y = get_dataset_by_key(json_datasets, "BEHAVE_ACC_Y_PEAK", time=True)
-		times[3], acc_z = get_dataset_by_key(json_datasets, "BEHAVE_ACC_Z_PEAK", time=True)
+		mileage = [mil - np.min(mil) for mil in mileage]
+		consumed_measured = [mil - np.min(mil) for mil in consumed_measured]
 
-		acc = [np.sqrt(np.array(acc_x_i)**2 + np.array(acc_y_i)**2 + np.array(acc_z_i)**2) for 
-			acc_x_i, acc_y_i, acc_z_i in zip(acc_x, acc_y, acc_z)]
+		# consts = [0.00018621021365193117, 0.00008486945108872097]
+		# meas_noise = [15, 12]
+		# proc_noise = [0.5, 0.3]
 
-		plt.subplot(211)
-		plot.plot(times[0], crash, xlab = "Time", ylab="Crash event", title="CRASH")
-		plt.subplot(212)
-		plot.plot(times[1], acc, xlab = "Time", ylab="Acceleration", title="ACC")
+		# consumed_kalman = [np.zeros(len(times[0][k])) for k in range(len(times[0]))]
+		# consumed_var = [np.zeros(len(times[0][k])) for k in range(len(times[0]))]
 
+		# # Kalman filter for consumed fuel
+		# for k in range(len(times[0])):
+		# 	t0 = times[1][k][0]
+		# 	consumed_kalman[k][0] = 0
+		# 	consumed_var[k][0] = 0
 
+		# 	const = consts[k]
+		# 	for i in range(1, len(times[0][k])):
+				
+		# 		# Prediction step
+		# 		dt = (times[0][k][i]-times[0][k][i-1]).total_seconds()
+		# 		consumed_kalman[k][i] = consumed_kalman[k][i-1] + rpm_odb[k][i]*dt*const
+		# 		consumed_var[k][i] = consumed_var[k][i] + proc_noise[k]*dt
 
+		# 		# Search for the measurement closest to the current prediction
+		# 		try:
+		# 			j = find_lt(times[1][k], times[0][k][i])
+		# 		except:
+		# 			continue
+
+		# 		dt_betw_meas_and_rpm = (times[1][k][j] - times[0][k][i]).total_seconds()
+
+		# 		# Correction
+		# 		kgain = consumed_var[k][i] / (consumed_var[k][i] + meas_noise[k]*dt_betw_meas_and_rpm)
+		# 		consumed_kalman[k][i] += kgain*(consumed_measured[k][j] - consumed_kalman[k][i])
+		# 		consumed_var[k][i] = (1 - kgain)*consumed_var[k][i]
+
+		times1_float = [np.array([(time - t[0]).total_seconds() for time in t]) for t in times[1]]
+		times2_float = [np.array([(time - t[0]).total_seconds() for time in t]) for t in times[2]]
+		mileage_interp = [np.interp(times1_float[k], times2_float[k], mileage[k]) for k in range(len(consumed_measured))]	
+
+		# to be investigated why 10e-2 works, instead of 10e-3
+		rate = [10e-2*consumed_measured[k]/(mileage_interp[k] + np.finfo(float).eps)  for k in range(len(consumed_measured))]
+
+		for k, car_id in enumerate(car_ids):
+			with open("{}_fuelrate.json".format(car_id), "w") as f:
+				data = [(int(time.mktime(x.timetuple())), y) for x, y in zip(times[1][k], rate[k])]
+				f.write(json.dumps(data))
 			
